af062e561f9d7e96788a0b677540e5ef314f0d2b
Diablo785 <96918963+Diablo785@users.noreply.github.com>
2023-11-07 13:54:33 +0200

    Add files via upload

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,19 @@
+import '../css/SideBar.css';
+
+function SideBar() {
+  return (
+    <div className="SideBar">
+        <div className="TopCont">
+
+        </div>
+        <div className="MidCont">
+        
+        </div>
+        <div className="BotCont">
+        
+        </div>
+    </div>
+  );
+}
+
+export default SideBar;
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,15 @@
+import '../css/Tasks.css';
+import SideBar from '../components/SideBar';
+
+function Tasks() {
+  return (
+    <>
+      <SideBar/>
+      <div className="Tasks">
+
+      </div>
+    </>
+  );
+}
+
+export default Tasks;
