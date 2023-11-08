ad38b930cbdc8e347f97319367e06420781eb145
Diablo785 <96918963+Diablo785@users.noreply.github.com>
2023-11-08 11:36:39 +0200

    Add files via upload

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,17 +1,130 @@
+import React, { useState } from 'react';
 import '../css/SideBar.css';
 
 function SideBar() {
-  return (
-    <div className="SideBar">
-        <div className="TopCont">
+  const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
+  const [isTasksOn, setIsTasksOn] = useState(true);
+  const [isCalendarOn, setIsCalendarOn] = useState(false);
+  const [isAccountOn, setIsAccountOn] = useState(false);
+  const [isSettingsOn, setIsSettingsOn] = useState(false);
 
+  const handleTasksButtonClick = () => {
+    if (!isTasksOn) {
+      setIsTasksOn(true);
+      setIsCalendarOn(false);
+      setIsAccountOn(false);
+      setIsSettingsOn(false);
+    }
+  };
+
+  const handleCalendarButtonClick = () => {
+    if (!isCalendarOn) {
+      setIsCalendarOn(true);
+      setIsTasksOn(false);
+      setIsAccountOn(false);
+      setIsSettingsOn(false);
+    }
+  };
+
+  const handleAccountButtonClick = () => {
+    if (!isAccountOn) {
+      setIsAccountOn(true);
+      setIsTasksOn(false);
+      setIsCalendarOn(false);
+      setIsSettingsOn(false);
+    }
+  };
+
+  const handleSettingsButtonClick = () => {
+    if (!isSettingsOn) {
+      setIsSettingsOn(true);
+      setIsTasksOn(false);
+      setIsCalendarOn(false);
+      setIsAccountOn(false);
+    }
+  };
+
+  const TasksButtonStyle = {
+    color: isTasksOn ? '#035A3B' : '#D9D9D9',
+  };
+
+  const CalendarButtonStyle = {
+    color: isCalendarOn ? '#035A3B' : '#D9D9D9',
+  };
+
+  const AccountButtonStyle = {
+    color: isAccountOn ? '#035A3B' : '#D9D9D9',
+  };
+
+  const SettingsButtonStyle = {
+    color: isSettingsOn ? '#035A3B' : '#D9D9D9',
+  };
+
+  const SidebarStyle = {
+    width: isSidebarCollapsed ? '0px' : '250px',
+  };
+
+  return (
+    <div style={SidebarStyle} className={`SideBar ${isSidebarCollapsed ? 'collapsed' : ''}`}>
+      <div className="TopCont">
+        <button
+          className={`CollapseArrow ${isSidebarCollapsed ? '>' : '<'}`}
+          onClick={() => setIsSidebarCollapsed(!isSidebarCollapsed)}
+        >
+        </button>
+        <div className="WelcomeUserCont">
+          <h4>Hello,</h4>
+          <h5>Andrejs!</h5>
         </div>
-        <div className="MidCont">
-        
+        <div className="TasksAndCalendarCont">
+          <button
+            className={`TasksButton ${isTasksOn ? 'ActiveButton' : ''}`}
+            style={TasksButtonStyle}
+            onClick={handleTasksButtonClick}
+          >
+            <div className={`Circle ${isTasksOn ? 'SlideRight' : 'SlideLeft'}`}></div>
+            <h6>Tasks</h6>
+          </button>
+          <button
+            className={`CalendarButton ${isCalendarOn ? 'ActiveButton' : ''}`}
+            style={CalendarButtonStyle}
+            onClick={handleCalendarButtonClick}
+          >
+            <div className={`Circle ${isCalendarOn ? 'SlideRight' : 'SlideLeft'}`}></div>
+            <h6>Calendar</h6>
+          </button>
         </div>
-        <div className="BotCont">
-        
+      </div>
+      <div className="MidCont">
+        <div className="CircleWithPlusCont">
+          <p>Create New Task</p>
+          <button className="MidContCircle">
+            <div className="CrossSign">
+              <div className="VerticalLine"></div>
+              <div className="HorizontalLine"></div>
+            </div>
+          </button>
         </div>
+      </div>
+      <div className="BotCont">
+        <button
+          className={`AccountButton ${isAccountOn ? 'ActiveButton' : ''}`}
+          style={AccountButtonStyle}
+          onClick={handleAccountButtonClick}
+        >
+          <div className={`Circle ${isAccountOn ? 'SlideRight' : 'SlideLeft'}`}></div>
+          <h6>Account</h6>
+        </button>
+        <button
+          className={`SettingsButton ${isSettingsOn ? 'ActiveButton' : ''}`}
+          style={SettingsButtonStyle}
+          onClick={handleSettingsButtonClick}
+        >
+          <div className={`Circle ${isSettingsOn ? 'SlideRight' : 'SlideLeft'}`}></div>
+          <h6>Settings</h6>
+        </button>
+        <button className="LogoutButton">Logout</button>
+      </div>
     </div>
   );
 }
