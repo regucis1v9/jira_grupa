2c01d6783c951f0e0b9cd89584703675399a1012
Diablo785 <96918963+Diablo785@users.noreply.github.com>
2023-11-17 00:21:08 +0200

    Add files via upload

--- api.php
+++ api.php
@@ -2,7 +2,7 @@
 // api.php
 
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, DELETE"); // Include DELETE method
+header("Access-Control-Allow-Methods: PUT, POST, GET, DELETE, OPTIONS"); // Include DELETE method
 header("Access-Control-Allow-Headers: Content-Type");
 
 
@@ -44,8 +44,32 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $stmt->close();
 } elseif ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['fetchTasks'])) {
-    // Handling GET requests for fetching tasks
-    $sql = "SELECT * FROM tasks";
+    $sortBy = $_GET['sort'] ?? 'due_date_asc'; // Default sorting option
+    
+    switch ($sortBy) {
+        case 'due_date_asc':
+            $orderBy = 'due_date ASC';
+            break;
+        case 'due_date_desc':
+            $orderBy = 'due_date DESC';
+            break;
+        case 'status_finished':
+            $orderBy = 'status = "Finished" DESC, status = "On Hold" DESC';
+            break;
+        case 'status_on_hold':
+            $orderBy = 'status = "On Hold" DESC, status = "Finished" DESC';
+            break;
+        case 'status_active':
+            $orderBy = 'status = "Active" DESC, status = "Finished" DESC, status = "On Hold" DESC';
+            break;
+        default:
+            $orderBy = 'due_date ASC'; // Default sorting by due date
+            break;
+    }
+
+    // Handling GET requests for fetching tasks with sorting
+    $sql = "SELECT * FROM tasks ORDER BY $orderBy"; // Modify your SQL query accordingly
+
     $result = $conn->query($sql);
 
     $tasks = [];
@@ -71,8 +95,32 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 
     $stmt->close();
-} else {
-    echo json_encode(array("message" => "Invalid request method"));
+} elseif ($_SERVER['REQUEST_METHOD'] === 'PUT' && isset($_GET['updateTask'])) {
+    $taskId = $_GET['updateTask'];
+    $requestData = json_decode(file_get_contents('php://input'), true);
+
+    // Validate if the required fields are empty
+    if (empty($requestData['title']) || empty($requestData['description']) || empty($requestData['due_date'])) {
+        echo json_encode(array("error" => "Please fill in all required fields"));
+        exit; // Stop execution if any field is empty
+    }
+
+    $title = $requestData['title'];
+    $description = $requestData['description'];
+    $due_date = $requestData['due_date'];
+    $status = $requestData['status'] ?? 'Active'; // Set status to 'Active' by default
+
+    // Update task data in the database
+    $stmt = $conn->prepare("UPDATE tasks SET title=?, description=?, due_date=?, status=? WHERE id=?");
+    $stmt->bind_param("ssssi", $title, $description, $due_date, $status, $taskId);
+
+    if ($stmt->execute()) {
+        echo json_encode(array("message" => "Task updated successfully"));
+    } else {
+        echo json_encode(array("message" => "Failed to update task"));
+    }
+
+    $stmt->close();
 }
 
 
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,22 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import '../css/Tasks.css';
 
 function Tasks() {
   const [tasks, setTasks] = useState([]);
+  const [editTask, setEditTask] = useState(null);
+  const [sortBy, setSortBy] = useState('due_date');
 
-useEffect(() => {
-  fetch('http://localhost/api/api.php?fetchTasks', {
-    method: 'GET'
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log('Fetched data:', data);
-      setTasks(data);
+  useEffect(() => {
+    fetch(`http://localhost/api/api.php?fetchTasks&sort=${sortBy}`, {
+      method: 'GET'
     })
-    .catch((error) => {
-      console.error('Error fetching tasks:', error);
-    });
-}, []);
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Fetched data:', data);
+        // Check the structure of the fetched data and set it accordingly
+        if (Array.isArray(data)) {
+          setTasks(data);
+        } else if (typeof data === 'object') {
+          // If data is an object, convert it to an array
+          setTasks([data]);
+        } else {
+          console.error('Invalid data format:', data);
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching tasks:', error);
+      });
+  }, [sortBy]);
+  
 
 
 const deleteTask = (taskId) => {
@@ -42,9 +53,88 @@ const deleteTask = (taskId) => {
     });
 };
 
+
+const handleSaveChanges = async () => {
+  try {
+    if (editTask) {
+      const response = await fetch(`http://localhost/api/api.php?updateTask=${editTask.id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(editTask),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to update task');
+      }
+
+      // Fetch the updated tasks after successful update
+      fetch('http://localhost/api/api.php?fetchTasks', {
+        method: 'GET'
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log('Fetched data:', data);
+          // Check the structure of the fetched data and set it accordingly
+          if (Array.isArray(data)) {
+            setTasks(data);
+          } else if (typeof data === 'object') {
+            // If data is an object, convert it to an array
+            setTasks([data]);
+          } else {
+            console.error('Invalid data format:', data);
+          }
+        })
+        .catch((error) => {
+          console.error('Error fetching tasks:', error);
+        });
+
+      handleCloseEdit(); // Close the edit modal after saving changes
+    }
+  } catch (error) {
+    console.error('Error updating task:', error);
+  }
+};
+
+
+const handleEdit = (task) => {
+  setEditTask(task);
+};
+
+const handleCloseEdit = () => {
+  setEditTask(null);
+};
+
+const handleSortChange = (criteria) => {
+  setSortBy(criteria);
+};
+
+const handleStatusChange = () => {
+  const statusOptions = ['Active', 'On Hold', 'Finished'];
+  const currentStatusIndex = statusOptions.indexOf(editTask.status);
+  const nextStatusIndex = (currentStatusIndex + 1) % statusOptions.length; // Cycle through the options
+
+  const updatedEditTask = {
+    ...editTask,
+    status: statusOptions[nextStatusIndex]
+  };
+  setEditTask(updatedEditTask);
+};
+
 return (
   <div className="Tasks">
-    <div className="TasksCont">
+    <div className="SortOptions">
+      <label>Sort By:</label>
+      <select onChange={(e) => handleSortChange(e.target.value)}>
+        <option value="due_date_asc">Due Date (Newest to Oldest)</option>
+        <option value="due_date_desc">Due Date (Oldest to Newest)</option>
+        <option value="status_finished">Sort by Finished First</option>
+        <option value="status_on_hold">Sort by On Hold First</option>
+        <option value="status_active">Sort by Active First</option> {/* New option */}
+      </select>
+    </div>
+    <div className="TasksCont" style={{ display: editTask ? 'none' : 'block' }}>
       <div className="TableWrapper">
         <table>
           <thead>
@@ -60,16 +150,18 @@ return (
           <tbody>
             {tasks.map((task) => (
               <tr key={task.id}>
-              
                 <td>{task.id}</td>
                 <td>{task.title}</td>
                 <td>{task.description}</td>
                 <td>{task.due_date}</td>
-                <td>{task.status}</td>
-                
+                <td className={task.status === 'Active' ? 'Active' : task.status === 'On Hold' ? 'OnHold' : 'Finished'}>
+                  {task.status}
+                </td>
                 <td>
                   <div className="buttons-container">
-                    <button className="Edit">Edit</button>
+                    <button className="Edit" onClick={() => handleEdit(task)}>
+                      Edit
+                    </button>
                     <button className="Delete" onClick={() => deleteTask(task.id)}>
                       Delete
                     </button>
@@ -81,6 +173,39 @@ return (
         </table>
       </div>
     </div>
+
+    {/* Edit modal */}
+    {editTask && (
+      <div className="EditModal">
+        <h2>Edit Task</h2>
+        
+        <label>Title:</label>
+        <input
+          type="text"
+          value={editTask.title}
+          onChange={(e) => setEditTask({ ...editTask, title: e.target.value })}
+        />
+        <label>Description:</label>
+
+        <textarea
+          value={editTask.description}
+          onChange={(e) => setEditTask({ ...editTask, description: e.target.value })}
+        ></textarea>
+
+        <label>Due Date:</label>
+        <input
+          type="date"
+          value={editTask.due_date}
+          onChange={(e) => setEditTask({ ...editTask, due_date: e.target.value })}
+        />
+        <label>Status:</label>
+        <button className={editTask.status === 'Active' ? 'Active' : editTask.status === 'On Hold' ? 'OnHold' : 'Finished'} onClick={handleStatusChange}>{editTask.status}</button>
+        <div>
+          <button className="SaveChanges" onClick={handleSaveChanges}>Save changes</button>
+          <button className="Cancel" onClick={handleCloseEdit}>Cancel</button>
+        </div>
+      </div>
+    )}
   </div>
 );
 }
